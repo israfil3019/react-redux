b1d75dec5706e387d5fb7bea35fc9c4208272e94
israfil <israfil3019@gmail.com>
2021-05-27 14:35:56 +0300

    finish redux lesson

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increase, decrease, reset } from "./redux/actions/actions";
 
 function App() {
-  return <div className="App"></div>;
+  const { counter } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Counter</h2>
+        <div>
+          <h1>{counter}</h1>
+        </div>
+        <div>
+          <button onClick={() => dispatch(increase())}>increase</button>
+          <button onClick={() => dispatch(reset())}>reset</button>
+          <button onClick={() => dispatch(decrease())}>decrease</button>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { counterStore } from "./redux";
+import { Provider } from "react-redux";
+
+const store = counterStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
